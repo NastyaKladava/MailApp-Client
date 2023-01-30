182f4ba67c213658ec1c026bfa75c225971226a3
Nastya Kladova <nstsologub@gmail.com>
2023-01-30 13:12:18 +0100

    change base url

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://mailapp-server-production.up.railway.app",
   headers: {
     "Content-type": "application/json",
   },
